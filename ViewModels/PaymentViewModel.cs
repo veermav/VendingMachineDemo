0a7284c317c8784a92057e86ab22e26b3156cb7c
Veeraj <veeraj.perilla@factset.com>
2018-05-17 15:58:19 +0530

    adding files

---
+++ ViewModels/PaymentViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineTutorial.ViewModels
+{
+    public class PaymentViewModel : ObservableObject
+    {
+        //customer Info
+        private double _total;
+        private double _inserted;
+        private double _change;
+
+        private double _bankTotal;
+
+        public double Total
+        {
+            get { return _total; }
+            set
+            {
+                _total = value;
+                OnPropertyChanged("Total");
+            }
+        }
+
+        public double Inserted
+        {
+            get { return _inserted; }
+            set
+            {
+                _inserted = value;
+                OnPropertyChanged("Inserted");
+            }
+        }
+
+        public double Change
+        {
+            get { return _change; }
+            set
+            {
+                _change = value;
+                OnPropertyChanged("change");
+            }
+        }
+
+        public double BankTotal 
+        {
+            get { return _bankTotal; }
+            set
+            {
+                _bankTotal = value;
+                OnPropertyChanged("BankTotal");
+            }
+        }
+
+        public PaymentViewModel()
+        {
+            Total = 0;
+            Inserted = 0;
+            Change = 0;
+            BankTotal = 0;
+        }
+
+        //insert money
+
+        public void Insert(double value)
+        {
+            Inserted += value;
+        }
+
+        public void SelectedPrice(double value)
+        {
+            Total = value;
+        }
+
+        public bool Confirm()
+        {
+            if (Inserted >= Total)
+                return true;
+            return false;
+        }
+
+        public void Pay()
+        {
+            Change = Total - Inserted;
+            BankTotal += Total;
+            Inserted = 0;
+            Total = 0;
+
+        }
+
+        public void Collect()
+        {
+            Console.WriteLine("Collected paymets : $" + BankTotal);
+            BankTotal = 0;
+        }
+
+    }
+}
---
+++ ViewModels/ProductViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VendingMachineTutorial.Models;
+
+namespace VendingMachineTutorial.ViewModels
+{
+    public class ProductViewModel : ObservableObject
+    {
+        //model of the Product view Model
+        private VendingItem _model;
+        //Maximum number of items allowed 
+        private const int _maxQuantity = 10;
+
+        private int _quantity;
+
+        public int id
+        {
+            get { return _model.Id; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            private set
+            {
+                _quantity = value; 
+                OnPropertyChanged("OutofStockMessage");
+                OnPropertyChanged("InventoryDisplay");
+                OnPropertyChanged("Quantity");
+            }
+        }
+
+        // Ex : "Regular Soda : 7"
+        public string InventoryDisplay
+        {
+            get { return _model.Name + ":" + _quantity; }
+        }
+        //return a copy of this model.
+
+        public VendingItem Information
+        {
+            get { return _model; }
+        }
+        //out of stock
+
+        public Visibility OutofStockMessage
+        {
+            get
+            {
+                if (Quantity > 0)
+                    return Visibility.Hidden;
+                return Visibility.Visible;
+            }
+        }
+
+        public ProductViewModel(int id , string name , double price)
+        {
+              _model = new VendingItem(id, name, price);
+              Quantity = 0;
+        }
+
+        public int Refill()
+        {
+            var amount = _maxQuantity - Quantity;
+            Quantity += amount;
+            return amount;
+        }
+
+        public int Empty()
+        {
+            var amount = Quantity;
+            Quantity = 0;
+            return amount;
+        }
+
+        public bool Dispense()
+        {
+            if (Quantity > 0)
+            {
+                Quantity--;
+                return true;
+            }
+            return false;
+        }
+    }
+}
